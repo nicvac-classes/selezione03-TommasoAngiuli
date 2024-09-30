97e160d22c5176d5ffc75efed2a6e97a73c9b3eb
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2024-09-30 20:46:21 +0000

    selezione 03

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int num1, num2, max;
+    cout << "inserisci primo numero" << endl;
+    cin >> num1;
+    cout << "inserisci secondo numero" << endl;
+    cin >> num2;
+    if (num1 > num2) {
+        max = num1;
+    } else {
+        max = num2;
+    }
+    cout << "il numero più grande è " << max << endl;
+    return 0;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
